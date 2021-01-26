0e0a3b5ef672968840ba794ca8e1a54ffa1459d6
lvh666 <lvhao4748@163.com>
2021-01-26 23:36:47 +0800

    Menu-bug修复

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,50 +1,61 @@
-import React, { createContext, useState } from 'react'
-import classNames from 'classnames'
-// import MenuItem from './MenuItem'
+import React, { createContext, useState } from "react";
+import classNames from "classnames";
+import { IMenuItemProps } from "./MenuItem";
 
-type MenuMode = 'horizontal' | 'vertical'
-type SelectCallback = (selectedIndex: number) => void
+type MenuMode = "horizontal" | "vertical";
+type SelectCallback = (selectedIndex: number) => void;
 export interface IMenuProps {
-    defaultIndex?: number;
-    className?: string;
-    mode?: MenuMode;
-    style?: React.CSSProperties;
-    onSelect?: SelectCallback;
+  defaultIndex?: number;
+  className?: string;
+  mode?: MenuMode;
+  style?: React.CSSProperties;
+  onSelect?: SelectCallback;
 }
 
 interface IMenuContext {
-    index: number;
-    onSelect?: SelectCallback;
+  index: number;
+  onSelect?: SelectCallback;
 }
 
-export const MenuContext = createContext<IMenuContext>({index: 0})
+export const MenuContext = createContext<IMenuContext>({ index: 0 });
 const Menu: React.FC<IMenuProps> = (props) => {
-    const { className, mode, style, children, defaultIndex, onSelect } = props
-    const [ currentActive, setActive ] = useState(defaultIndex)
-    const classes = classNames('viking-menu', className, {
-        'menu-vertical': mode === 'vertical'
-    })
-    const handleClick = (index: number) => {
-        setActive(index)
-        if(onSelect) {
-            onSelect(index)
-        }
+  const { className, mode, style, children, defaultIndex, onSelect } = props;
+  const [currentActive, setActive] = useState(defaultIndex);
+  const classes = classNames("viking-menu", className, {
+    "menu-vertical": mode === "vertical",
+  });
+  const handleClick = (index: number) => {
+    setActive(index);
+    if (onSelect) {
+      onSelect(index);
     }
-    const passedContext: IMenuContext = {
-        index: currentActive ? currentActive : 0,
-        onSelect: handleClick,
-    }
-    return (
-        <ul className = {classes} style = {style} data-testid='test-menu'>
-            <MenuContext.Provider value={passedContext}>
-            {children}
-            </MenuContext.Provider>
-        </ul>
-    )
-}
+  };
+  const passedContext: IMenuContext = {
+    index: currentActive ? currentActive : 0,
+    onSelect: handleClick,
+  };
+  const renderChildren = () => {
+    return React.Children.map(children, (child, index) => {
+      const childElement = child as React.FunctionComponentElement<IMenuItemProps>;
+      const { displayName } = childElement.type
+      if( displayName === 'MenuItem') {
+          return child
+      } else {
+          console.error('Waring: Menu has a child which is not a MenuItem')
+      }
+    });
+  };
+  return (
+    <ul className={classes} style={style} data-testid="test-menu">
+      <MenuContext.Provider value={passedContext}>
+        {renderChildren}
+      </MenuContext.Provider>
+    </ul>
+  );
+};
 Menu.defaultProps = {
-    defaultIndex: 0,
-    mode: 'horizontal'
-}
+  defaultIndex: 0,
+  mode: "horizontal",
+};
 
 export default Menu;
