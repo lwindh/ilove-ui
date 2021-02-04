441fa355626078876e491f7d616efd0eadef6e83
lvh666 <lvhao4748@163.com>
2021-02-04 19:48:38 +0800

    Alert组件测试

--- src/components/Alert/Alert.test.tsx
+++ src/components/Alert/Alert.test.tsx
@@ -13,15 +13,17 @@ const testProps: AlertProps = {
   onClose: jest.fn(),
 };
 
-let wrapper: RenderResult, alertComponent: HTMLElement,colsedComponent: HTMLElement;
+const closableProps: AlertProps = {
+    message: "test",
+    type: 'success',
+    closable: false, 
+}
+
+let wrapper: RenderResult, alertComponent: HTMLElement;
 describe("test Alert component", () => {
-  beforeEach(() => {
+  it("should render correct Alert based on default props", () => {
     wrapper = render(<Alert {...testProps} />);
     alertComponent = wrapper.getByTestId("test-alert");
-    colsedComponent = alertComponent.querySelector(":scope > Button")
-  });
-
-  it("should render correct Alert based on default props", () => {
     expect(alertComponent).toHaveClass("alert-info");
     expect(alertComponent).not.toHaveClass("alert-motion-leave alert-motion-leave-active");
     expect(wrapper.getByText("test")).toBeInTheDocument();
@@ -29,7 +31,15 @@ describe("test Alert component", () => {
       alertComponent.getElementsByClassName("alert-description")[0].innerHTML
     ).toEqual("");
     expect(alertComponent.querySelector(":scope > Button")).toBeInTheDocument();
-    fireEvent.click(colsedComponent)
+    fireEvent.click(alertComponent.querySelector(":scope > Button"))
+    expect(testProps.onClose).toHaveBeenCalled()
     expect(alertComponent).toHaveClass("alert-motion-leave alert-motion-leave-active");
   });
+
+  it("should render correct Alert based on closable props", () => {
+    wrapper = render(<Alert {...closableProps} />);
+    alertComponent = wrapper.getByTestId("test-alert");
+    expect(alertComponent).toHaveClass("alert-success");
+    expect(alertComponent.querySelector(":scope > Button")).not.toBeInTheDocument();
+  })
 });
