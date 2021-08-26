3d9402ec8d56473b779a3962294d541a6519e178
lvh666 <lvhao4748@163.com>
2021-08-26 23:29:37 +0800

    refactor

--- dist/components/Tabs/TabsItem.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export interface TabsItemProps {
-    /**选项卡头显示文字 */
-    label: any;
-    /**对应 defaultActiveKey */
-    index?: string;
-}
-export declare const TabsItem: React.FC<TabsItemProps>;
-export default TabsItem;
--- dist/components/Tabs/index.js
+++
@@ -1,5 +0,0 @@
-import Tabs from './Tabs';
-import TabsItem from './TabsItem';
-var TransTabs = Tabs;
-TransTabs.Item = TabsItem;
-export default TransTabs;
